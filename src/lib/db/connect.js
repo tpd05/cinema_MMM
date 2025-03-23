bc3186f90729b3bba55fbddfd3ba6c4ac3f172d7
Trần Phương Đoàn <doantran28092005@gmail.com>
2025-03-24 01:36:18 +0700

    Update connect.js

--- src/lib/db/connect.js
+++ src/lib/db/connect.js
@@ -1,14 +1,14 @@
+import mysql from 'mysql2';
+
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || 'localhost',
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || '',
-  database: process.env.MYSQLDATABASE || '',
-  port: process.env.MYSQLPORT ? parseInt(process.env.MYSQLPORT) : 3306,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: parseInt(process.env.MYSQLPORT),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : null,
 });
 
 export default pool;
-
